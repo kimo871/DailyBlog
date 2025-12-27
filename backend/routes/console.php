3d968be52fb1155139e261a85ed56c512d69b558
kimo871 <mk5391130@gmail.com>
2025-12-27 12:39:47 +0200

    feat : delete expired posts using scheduler + queue workers

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\DeleteExpiredPosts;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::job(new DeleteExpiredPosts)
+    ->everyMinute();
+
