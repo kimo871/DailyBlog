98e02580089f72163c0531db3e7bad9cf52b3fa9
kimo871 <mk5391130@gmail.com>
2025-12-28 16:15:45 +0200

    feat : implement toast for better ux + fallback to 404page for invalid routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
-// import { Toaster } from "@/components/ui/toaster";
-// import { Toaster as Sonner } from "@/components/ui/sonner";
-// import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "./components/ui/sonner";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
@@ -9,13 +8,13 @@ import { AuthProvider } from "./contexts/AuthContext";
 import PostDetail from "./pages/PostDetail";
 import EditPost from "./pages/EditPost";
 import CreatePost from "./pages/CreatePost";
+import { NotFound } from "./pages/NotFound";
 
 
 const App = () => (
     <AuthProvider>
-      {/* <TooltipProvider>
         <Toaster />
-        <Sonner position="top-right" /> */}
+        <Sonner position="top-right" /> 
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -24,13 +23,10 @@ const App = () => (
           <Route path="/posts/:id" element={<PostDetail />} />
           <Route path="/posts/new" element={<CreatePost />} />
           <Route path="/posts/:id/edit" element={<EditPost />} />
-          {/* <Route path="/profile" element={<Profile />} /> */}
-          {/* <Route path="/profile/:id" element={<Profile />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-      {/* </TooltipProvider> */}
     </AuthProvider>
 );
 
